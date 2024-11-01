892caedfe4a3f55d5d39bc6e32d25371a08fc239
RodriFN <floresnunezrodrigo8@gmail.com>
2024-11-01 11:02:06 -0500

    Creamos un metodo para calcular

--- cgi-bin/calcular.pl
+++ cgi-bin/calcular.pl
@@ -8,3 +8,39 @@ my $expresion = $cgi->param('expression');
 my $resultado = evaluar_expresion($expresion);
 
 print $cgi->header('text/html; charset=UTF-8');
+
+sub calcular {
+    my $expresion = shift;
+
+    while ($expresion =~ /(-?\d+\.?\d*)\s*([\*\/])\s*(-?\d+\.?\d*)/) {
+        my $operando1 = $1;
+        my $operador = $2;
+        my $operando2 = $3;
+        my $resultado;
+
+        if ($operador eq '*') {
+            $resultado = $operando1 * $operando2;
+        } elsif ($operador eq '/') {
+            $resultado = $operando1 / $operando2;
+        }
+
+        $expresion =~ s/\Q$operando1$operador$operando2\E/$resultado/;
+    }
+
+    while ($expresion =~ /(-?\d+\.?\d*)\s*([\+\-])\s*(-?\d+\.?\d*)/) {
+        my $operando1 = $1;
+        my $operador = $2;
+        my $operando2 = $3;
+        my $resultado;
+
+        if ($operador eq '+') {
+            $resultado = $operando1 + $operando2;
+        } elsif ($operador eq '-') {
+            $resultado = $operando1 - $operando2;
+        }
+
+        $expresion =~ s/\Q$operando1$operador$operando2\E/$resultado/;
+    }
+
+    return $expresion;
+}
